d156b1d228c2dba6e53734d6328cfd4de5b193b0
Andrey Gurev <Andrey@MacBook-Air-Andrey.local>
2017-04-08 00:47:54 +1000

    Add dormitory selection card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 import AuthCard from './AuthCard';
+import RoomFinder from './RoomFinder'
 
 class App extends Component {
     render() {
         return (
+            <div>
                 <AuthCard />
+                <RoomFinder />
+            </div>
         );
     }
 }
---
+++ src/RoomFinder.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+import './css/mainPageStyles.css'
+
+class RoomFinder extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col s12 m6 offset-m3">
+                        <div className="card">
+                            <div className="card-content">
+                                <span className="card-title black-text">Выберите общежитие</span>
+                                <div class="input-field col s12">
+                                    <select multiple>
+                                        <option value="" disabled selected>Номер общежития</option>
+                                        <option value="1">8.2</option>
+                                        <option value="2">8.1</option>
+                                        <option value="3">6.1</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="card-action">
+                                <input type="submit" className="btn" value="Применить"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default RoomFinder;
