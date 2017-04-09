e3066444cce76645b1affafc68c3aa9d65176575
KeltarHelviett <keltarhelviett@gmail.com>
2017-04-09 13:32:44 +1000

    Integrated firebase db

--- src/index.js
+++ src/index.js
@@ -5,6 +5,19 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import App from './App';
 
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyCpJWn3NVuV_sijmT8oCGyDR0vTPYhpRRQ",
+    authDomain: "dvhackathon2017.firebaseapp.com",
+    databaseURL: "https://dvhackathon2017.firebaseio.com",
+    projectId: "dvhackathon2017",
+    storageBucket: "dvhackathon2017.appspot.com",
+    messagingSenderId: "1061453765204"
+};
+
+firebase.initializeApp(config);
+window.db = firebase.database();
 injectTapEventPlugin();
 
 ReactDOM.render(
